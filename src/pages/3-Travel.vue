efcdcacd49508cf24b283e7fc24edfe3b14485ac
valentin.brajon <valentin.brajon@neoxam.com>
2024-08-16 05:58:40 +0200

    feat: stops

--- src/pages/3-Travel.vue
+++ src/pages/3-Travel.vue
@@ -11,11 +11,14 @@
     <ion-content>
       <div style="display: flex; flex-direction: column; height: 100%">
         <div style="display: flex; height: 85%">
-          <img style="max-width: 100%; height: auto; margin: auto" :src="currentChoice.image" />
+          <div style="display: flex; flex-direction: column; gap: 4px; padding: 20px" v-if="currentChoice.stops">
+            <h2 v-for="stop in currentChoice.stops">{{ stop }}</h2>
+          </div>
+          <img style="max-width: 100%; height: auto; margin: auto" :src="currentChoice.image" v-else />
         </div>
         <div style="display: flex; flex: 1">
           <h2 style="padding: 4px; margin: auto">{{ currentChoice.text }}</h2>
-          <ion-button @click="$router.push({ query: { step: currentStep + 1 } })" v-if="currentStep < photos.length">SUIVANT</ion-button>
+          <ion-button @click="$router.push({ query: { step: currentStep + 1 } })" v-if="currentStep < steps.length">SUIVANT</ion-button>
         </div>
       </div>
     </ion-content>
@@ -26,6 +29,6 @@
 import { computed } from "vue"
 const currentTrip = $state.trips[$route.params.id - 1] || {}
 const currentStep = computed(() => +($route.query.step || 1))
-const photos = computed(() => currentTrip.sequences.flatMap((v) => v.photos).filter((v) => v.image))
-const currentChoice = computed(() => photos.value[currentStep.value - 1])
+const steps = computed(() => currentTrip.sequences.flatMap((v) => (v.stops ? [v.photos[0], v, v.photos[1]] : v.photos)).filter((v) => v.stops || v.image))
+const currentChoice = computed(() => steps.value[currentStep.value - 1])
 </script>
