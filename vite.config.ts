20f62fb4cc5ec8bb0b1f7e22f8017deb4cdd708b
valentin.brajon <valentin.brajon@neoxam.com>
2024-09-13 14:45:48 +0200

    fix: icons

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,34 @@ export default defineConfig({
         },
       },
     }),
-    VitePWA({ registerType: "autoUpdate" }),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
     legacy(),
   ],
   resolve: {
