8fc63a99700a2c7989ba8864003d8c586fc6c787
valentin.brajon <valentin.brajon@neoxam.com>
2024-09-20 07:08:38 +0200

    feat: GPS data + stop progress animation

--- src/pages/2-Trip.vue
+++ src/pages/2-Trip.vue
@@ -29,7 +29,7 @@
 
         <ion-list v-if="currentStep === 3">
           <ion-item button detail="false" @click="next((state.sequence = i))" v-for="(sequence, i) in currentChoice">
-            {{ sequence.transport }}{{ sequence.stops ? ` (${sequence.stops.length} arrêts) [${sequence.stops[0]} > ${sequence.stops.at(-1)}]` : "" }}
+            {{ sequence.transport }}{{ sequence.stops ? ` (${sequence.stops.length} arrêts) [${sequence.stops[0].name} > ${sequence.stops.at(-1).name}]` : "" }}
           </ion-item>
         </ion-list>
 
@@ -206,12 +206,15 @@ const nexts = {
     state.sequences = $$(".detail-trip", html).map((el) => {
       const sequence = []
       $$("td:nth-child(1) > .item-line", el).forEach((el, i) => {
-        const before = $(".details span", el.parentElement.parentElement.previousElementSibling).firstChild.textContent.trim()
-        const intermediary = $$("ul li", el.parentElement.parentElement).map((el) => el.firstChild.textContent.trim())
-        const after = $(".details span", el.parentElement.parentElement.nextElementSibling).firstChild.textContent.trim()
+        const before = { name: $(".details span", el.parentElement.parentElement.previousElementSibling).firstChild.textContent.trim(), lat: state.from.Latitude, lng: state.from.Longitude }
+        const intermediary = $$("ul li", el.parentElement.parentElement).map((el) => {
+          const { lat, lng } = $("[data-lat]", el).dataset
+          return { name: el.firstChild.textContent.trim(), lat: +lat, lng: +lng }
+        })
+        const after = { name: $(".details span", el.parentElement.parentElement.nextElementSibling).firstChild.textContent.trim(), lat: state.to.Latitude, lng: state.to.Longitude }
         const stops = [before, ...intermediary, after].filter((v) => v)
-        sequence.push({ transport: `Je marche vers l'arrêt n°${i + 1} ${stops[0]}`, photos: [] })
-        sequence.push({ transport: `J'attend à l'arrêt n°${i + 1} ${stops[0]}`, photos: [] })
+        sequence.push({ transport: `Je marche vers l'arrêt n°${i + 1} ${stops[0].name}`, photos: [] })
+        sequence.push({ transport: `J'attend à l'arrêt n°${i + 1} ${stops[0].name}`, photos: [] })
         sequence.push({ transport: `Je monte dans le ${el.innerText}`, stops, photos: [] })
       })
       sequence.push({ transport: `Je marche vers ma destination`, photos: [] })
--- src/pages/3-Travel.vue
+++ src/pages/3-Travel.vue
@@ -11,8 +11,25 @@
     <ion-content>
       <div style="display: flex; flex-direction: column; height: 100%">
         <div style="display: flex; height: 85%">
-          <div style="display: flex; flex-direction: column; gap: 4px; padding: 20px" v-if="currentChoice.stops">
-            <h2 v-for="stop in currentChoice.stops">{{ stop }}</h2>
+          <!-- https://play.tailwindcss.com/90GckuhEBW -->
+          <div style="width: 100vw; overflow-x: auto" v-if="currentChoice.stops">
+            <div style="position: relative; display: flex; min-width: fit-content; align-items: center; justify-content: space-between; margin: 4rem">
+              <div
+                style="z-index: 10; margin: 4rem; height: 1rem; width: 1rem; display: flex; align-items: center; justify-content: center; border-radius: 9999px; background-color: white"
+                :style="[i === 0 && 'margin-left: 0', i === currentChoice.stops.length - 1 && 'margin-right: 0', i <= progress.number ? 'background-color: #2563eb' : 'border: 2px solid black;']"
+                v-for="(stop, i) in currentChoice.stops"
+                ref="stops"
+              >
+                <div
+                  style="position: absolute; z-index: 10; margin: -0.5rem; height: 1.5rem; width: 1.5rem; border-radius: 9999px; background-color: #2563eb"
+                  class="animate-ping"
+                  v-if="i === currentChoice.stops.length - 1 && progress.number > i - 2"
+                ></div>
+                <div style="min-width: 10rem; min-height: 2rem; transform: translateY(-100%); text-align: center; text-wrap: balance">{{ stop.name }}</div>
+              </div>
+              <div style="position: absolute; left: 0; right: 0; top: 50%; height: 1rem; transform: translateY(-50%); border-radius: 9999px; background-color: #d1d5db"></div>
+              <div style="position: absolute; left: 0; right: 0; top: 50%; height: 1rem; transform: translateY(-50%); border-radius: 9999px; background-color: #93c5fd" :style="progress"></div>
+            </div>
           </div>
           <img style="max-width: 100%; max-height: 100%; height: auto; margin: auto" :src="$state.photos[currentChoice.id]" v-else />
         </div>
@@ -26,9 +43,34 @@
 </template>
 
 <script setup lang="ts">
-import { computed, watch } from "vue"
+import { ref, computed, watch, onMounted } from "vue"
 const currentTrip = $state.trips[$route.params.id - 1] || {}
 const currentStep = computed(() => +($route.query.step || 1))
 const steps = computed(() => currentTrip.sequences.flatMap((v) => (v.stops ? [v.photos[0], v, v.photos[1]].filter((v) => v) : v.photos)).filter((v) => v.stops || v.id))
 const currentChoice = computed(() => steps.value[currentStep.value - 1])
+const timer = ref(0)
+setInterval(() => timer.value++, 20)
+const progress = computed(() => {
+  const number = Math.floor(timer.value / 100)
+  const percentage = (timer.value % 100) / 100
+  return { number, percentage, width: `${9 * number + 9 * percentage + 1}rem`, maxWidth: "100%" }
+})
+const stops = ref([])
+watch(
+  () => progress.value.number,
+  (n) => stops.value[n]?.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" })
+)
 </script>
+
+<style>
+@keyframes ping {
+  75%,
+  100% {
+    transform: scale(2);
+    opacity: 0;
+  }
+}
+.animate-ping {
+  animation: ping 1s cubic-bezier(0, 0, 0.2, 1) infinite;
+}
+</style>
